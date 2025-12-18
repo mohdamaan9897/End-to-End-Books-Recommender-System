a55dc881f1c137a0cbd819313615bec305040328
mohdamaan9897 <khanamaan44696@gmail.com>
2025-12-18 22:04:56 +0530

    constant and config yaml is updated

---
+++ books_recommender/constant/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+ROOt_DIR = os.getcwd()
+# Main config file path
+CONFIG_FOLDER_NAME = "config"
+CONFIG_FILE_NAME = "config.yaml"
+CONFIG_FILE_PATH = os.path.join(ROOt_DIR, CONFIG_FOLDER_NAME, CONFIG_FILE_NAME)
